d93ed2c58f179f9cb63a1e99e80f349c28734570
DiegoHaroVivanco <diego.harovivanco@gmail.com>
2022-08-31 02:07:32 -0300

    ejercicios 1 y 2

---
+++ ejercicio_2/src/main/java/ar/edu/unlp/info/oo1/objetos_uno/Balanza.java
@@ -0,0 +1,43 @@
+package ar.edu.unlp.info.oo1.objetos_uno;
+
+public class Balanza {
+	
+	private int cantidadDeProductos;
+	private double precioTotal;
+	private double pesoTotal;
+	
+	
+	public void ponerEnCero() {
+		this.cantidadDeProductos = 0;
+		this.precioTotal = 0; 
+		this.pesoTotal = 0;
+	}
+	
+	public void agregarProducto(Producto producto) {
+		this.cantidadDeProductos++;
+		this.precioTotal += producto.getPrecio();
+		this.pesoTotal += producto.getPeso();
+	}
+	
+	public Ticket emitirTicket() {
+		return new Ticket(this.cantidadDeProductos, this.pesoTotal, this.precioTotal);
+	}
+	
+
+	public int getCantidadDeProductos() {
+		return cantidadDeProductos;
+	}
+
+	public double getPrecioTotal() {
+		return precioTotal;
+	}
+
+	public double getPesoTotal() {
+		return pesoTotal;
+	}
+	
+	
+	
+	
+	
+}
